6d7b50da378799c13f5af9d232123db41ea4365f
Xavie <chenot.xaviier@gmail.com>
2020-11-17 12:51:58 +0100

    update exemple json with python

--- python-container/src/exempleJson.py
+++ python-container/src/exempleJson.py
@@ -4,14 +4,18 @@ import json
 with open('exemple.json') as f:
   data = json.load(f)
 
+# output --> data json
 print(data)
 print('\n')
+# output --> element 'animals' from data json
 print(data['animals'])
 print('\n')
-data['animals']
+# output --> len of the element 'animals' from data json
 print(len(data['animals']))
 
 for i in range(len(data['animals'])):
+    # output --> print the i element 'animals' from data json
     print(data['animals'][i])
     data2=json.loads(json.dumps(data['animals'][i]))
+    # output --> print the element 'species' from the i element of 'animals' data json
     print(data2['species'])
